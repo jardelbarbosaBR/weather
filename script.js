54ef73a74528bf4eabf4c4bb40a8083b83658e15
Jardel Barbosa <jardelsantos1111@gmail.com>
2023-12-05 18:07:57 -0300

    Conexão API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Isabela")
+const apikey = "28838148eef2423e1cb56ed91262f783"
+const inputCidade = document.getElementById('buscarCidade')
+const celsus = document.getElementById('numberCelsus')
+const tempoAtual = document.getElementById('tempoAtualizado')
+const nomeDacidade = document.getElementById('nomecidade')
+const sessaoTermica = document.getElementById('termica')
+const humildade = document.getElementById('humidadeSessao')
+
+inputCidade.addEventListener('keydown', function(e){
+    if(e.keyCode == 13){
+        buscarInforTempo(inputCidade.value)
+    }
+})
+
+function buscarInforTempo(cidade){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apikey}&units=metric&lang=pt_br`)
+    .then(r => r.json())
+    .then(tempo =>{
+        console.log(tempo)
+    })
+    .catch((erro) => {console.log("Aconteceu um erro" + erro)})
+}
