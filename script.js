7c0b63031fd771cf0de5b8bf18998acccfcb6235
Jardel Barbosa <jardelsantos1111@gmail.com>
2023-12-05 21:31:07 -0300

    Add func

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const tempoAtual = document.getElementById('tempoAtualizado')
 const nomeDacidade = document.getElementById('nomecidade')
 const sessaoTermica = document.getElementById('termica')
 const humildade = document.getElementById('humidadeSessao')
+const icone = document.getElementById('iconeTempo')
 
 inputCidade.addEventListener('keydown', function(e){
     if(e.keyCode == 13){
@@ -23,8 +24,10 @@ function buscarInforTempo(cidade){
 }
 
 function exibirInfor(tempo){
-    
+    icone.setAttribute("src", `https://openweathermap.org/img/wn/${tempo.weather[0].icon}@4x.png`)
     celsus.innerText = parseInt(tempo.main.temp)
     tempoAtual.innerText = tempo.weather[0].description
     nomeDacidade.innerText = tempo.name
+    sessaoTermica.innerText = `${parseInt(tempo.main.feels_like)}°C`
+    humildade.innerText = `${tempo.main.humidity}%`
 }
